11efe7897854007deb0964a46fbec84789afdd22
NeilHundia <hundianeil@gmail.com>
2025-01-22 17:50:13 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import SolarBot from "./Components/SolarBot";
+import Navbar from './Navbar';
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <SolarBot />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Navbar.css"; // For Navbar-specific styling
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="navbar-left">
+        <h1 className="company-name">Renergy</h1>
+      </div>
+      <div className="navbar-right">
+        <button className="nav-btn">Home</button>
+        <button className="nav-btn">Products</button>
+        <button className="nav-btn">About Us</button>
+        <button className="nav-btn">Contact Us</button>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
